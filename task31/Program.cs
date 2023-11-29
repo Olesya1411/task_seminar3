4de99c0944b3756f2d1dddb191bf34075fc61dca
Olesya1411 <blinova.olesya@inbox.ru>
2023-11-29 21:49:37 +0400

    add task31

---
+++ task31/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100.
+// Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+
+
+Console.WriteLine("Введите длину массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+Random r = new Random();
+int startRange = 20;
+int endRange = 90;
+
+
+for (int i = 0; i < size; i++)
+    array[i] = r.Next(1, 101);
+for (int i = 0; i < size; i++)
+    Console.Write(array[i] + "  ");
+
+
+Console.WriteLine();
+
+int count = 0;
+for (int i = 0; i < size; i++)
+    if (array[i] >= startRange && array[i] <= endRange)
+        count++;
+
+
+    Console.Write($"Количество чисел в диапазоне от {startRange} до {endRange}: {count}");
