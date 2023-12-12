97aea5d1d46e1ad6e30c2b5b52ea39a69dfb5328
Olesya1411 <blinova.olesya@inbox.ru>
2023-12-12 20:16:25 +0400

    add task71

---
+++ task71/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от M до N.
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNumbers(int M, int N)
+{
+    if (M > N)
+        return;
+    Console.Write(M + "\t");
+    PrintNumbers(M + 1, N);
+}
+
+int M = ReadInt("Введите число M: ");
+int N = ReadInt("Введите число N: ");
+PrintNumbers(M, N);
