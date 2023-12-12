0479a8fe99452ac37f3f6f96ae4631a348bce399
Olesya1411 <blinova.olesya@inbox.ru>
2023-12-12 22:20:46 +0400

    add task73

---
+++ task73/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+
+
+        public void Run()
+        {
+            int[] numbers = ArrayHelper.CreateIntArrayInRange(10, 1, 10);
+
+            Console.WriteLine(string.Join("; ", numbers));
+
+            PrintLastItem(numbers);
+        }
+        public void PrintLastItem(int[] numbers)
+        {
+            if(numbers.Length == 0)
+                return;
+
+            Console.Write($"{numbers.Last()} ");
+            PrintLastItem(numbers.Take(numbers.Length - 1).ToArray());
+        }
